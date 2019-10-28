9cae30e4ee4b3b24393d27a15fbb78cfdb2b01d0
KrolikOVSky <ovskykrolik@gmail.com>
2019-10-28 23:39:26 +0300

    add

--- src/encryption.java
+++ src/encryption.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class encryption{
-    /*Вывод на экран текста в формате Unicode изменения*/
+    /*Вывод на экран текста в формате Unicode new changes*/
     private static void unicodeList(char[] code) {
         arrayList2(code);
         System.out.println();
